81a15e53bdf1a1a8db6712c10f2fe4a0353b251d
ccbikai <ccbikai@qq.com>
2024-07-06 19:39:52 +0800

    feat: add content collections

---
+++ src/content/config.js
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const postCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		dateFormatted: z.string(),
+	}),
+});
+
+export const collections = {
+	post: postCollection,
+};
